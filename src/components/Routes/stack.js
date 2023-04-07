3c79d7545a6d50e874beed1369b983ecf9068dde
Breno Seren <breno@bizcommerce.com.br>
2023-04-07 14:13:24 -0300

    delete components1

--- src/components/Routes/stack.js
+++ src/components/Routes/stack.js
@@ -9,7 +9,8 @@ export default function Stack (){
 
 	return (
         <StackNavigator.Navigator initialRouteName = "Invite" screenOptions={{
-    headerShown: false
+    headerShown: false,
+    safeAreaInsets: { top: 0, bottom: 0 },
   }}>
           <StackNavigator.Screen 
                 name='Invite' 
