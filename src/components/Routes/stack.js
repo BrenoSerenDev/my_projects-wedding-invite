020273966183b1716d7e19c820fbd9ce896317ab
Breno Seren <breno@bizcommerce.com.br>
2023-04-16 19:24:57 -0300

    resolve some bugs in loading page

--- src/components/Routes/stack.js
+++ src/components/Routes/stack.js
@@ -2,15 +2,21 @@ import React from "react";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Invite from '../Invite'
 import Manual from '../Manual'
+import Splash from '../Splash'
+
 
 const StackNavigator = createNativeStackNavigator();
 
 export default function Stack (){
 
 	return (
-        <StackNavigator.Navigator initialRouteName = "Invite" screenOptions={{
+        <StackNavigator.Navigator style={{backgroundColor:'black'}} initialRouteName = "Splash" screenOptions={{
     headerShown: false,
   }}>
+          <StackNavigator.Screen 
+                name='Splash' 
+                component={Splash}
+            />
           <StackNavigator.Screen 
                 name='Invite' 
                 component={Invite}
